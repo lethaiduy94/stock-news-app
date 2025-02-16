102be0a3faedeb0aef809c00b9e6e3cf54b855c8
lethaiduy <lethaiduy94@gmail.com>
2025-02-16 22:47:18 +0900

    change AI module

--- app/api/translate/route.tsx
+++ app/api/translate/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-001" });
 
 export async function POST(request: Request) {
   if (!process.env.GEMINI_API_KEY) {
